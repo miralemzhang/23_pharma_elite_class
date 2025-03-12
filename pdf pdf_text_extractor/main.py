02064d281046d1508c19df3f6b4b696f7aa85880
mlmzzzooo <162582605+miralemzhang@users.noreply.github.com>
2025-03-12 20:04:19 +0800

    Update and rename line number remover.py to main.py

--- pdf pdf_text_extractor/main.py
+++ pdf pdf_text_extractor/main.py
@@ -1,10 +1,6 @@
 import pytesseract
 from pdf2image import convert_from_path
 import re
-import os
-
-# 用户需要修改此处以设置 Tesseract-OCR 的路径
-# pytesseract.pytesseract.tesseract_cmd = r'your_file_path'
 
 def extract_text_from_pdf(pdf_path):
     pages = convert_from_path(pdf_path, 300)
@@ -25,7 +21,7 @@ def process_pdf(pdf_path, output_path):
     
     print(f"清理后的文本已保存到 {output_path}")
 
-# 提示用户输入 PDF 文件路径和输出文本文件路径
-pdf_file_path = input("请输入 PDF 文件的路径：")
-output_text_path = input("请输入输出 txt 文件的路径：")
-process_pdf(pdf_file_path, output_text_path)
+if __name__ == "__main__":
+    pdf_file_path = input("请输入 PDF 文件的路径：")
+    output_text_path = input("请输入输出 txt 文件的路径：")
+    process_pdf(pdf_file_path, output_text_path)
